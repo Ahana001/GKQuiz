859182117ee955ad3a11dd8825647ed98242a638
Ankita <50478681+Ahana001@users.noreply.github.com>
2022-08-01 22:06:42 +0530

    refactor : added new question and answers

--- index.js
+++ index.js
@@ -7,47 +7,52 @@ var info = chalk.bold.blue;
 var score = 0;
 
 var userName = readlineSync.question("what is Your name? \n");
-log(info("welcome "+userName));
+log(info("welcome " + userName));
 
 var questionArray = [
-    {
-      question : "The language spoken by the people by Pakistan is ?",
-      options : ["Hindi","Palauan","Sindhi","Nauruan"],
-      answer : 2
-    },
-    {
-       question: "What is the state flower of Haryana?",
-      options : ["Lotus","Rhododendron","Golden Shower","Not declared"],
-       answer: 0
-    },
-    {
-       question: "In what state is Elephant Falls located?",
-       options : ["Mizoram","Orissa","Manipur","Meghalaya"],
-       answer: 3
-    },
-    {
-       question: "Name of the first university of India?",
-       options : [" Nalanda University"," Taxshila University"," Jawahar University"," Dronacharya University"],
-       answer: 0
-    }
+  {
+    question: "The language spoken by the people by Pakistan is ?",
+    options: ["Hindi", "Palauan", "Sindhi", "Nauruan"],
+    answer: 2
+  },
+  {
+    question: "What is the state flower of Haryana?",
+    options: ["Lotus", "Rhododendron", "Golden Shower", "Not declared"],
+    answer: 0
+  },
+  {
+    question: "In what state is Elephant Falls located?",
+    options: ["Mizoram", "Orissa", "Manipur", "Meghalaya"],
+    answer: 3
+  },
+  {
+    question: "Name of the first university of India?",
+    options: [" Nalanda University", " Taxshila University", " Jawahar University", " Dronacharya University"],
+    answer: 0
+  },
+  {
+    question: "Which is the largest coffee producing state of India?",
+    options: ["Kerala", "Tamil Nadu", "Karnataka", "Arunachal Pradesh"],
+    answer: 2
+  }
 ];
 
-function play(arr){
-  var userAns = readlineSync.keyInSelect(arr.options,arr.question);
-  if(userAns !== -1){
-    log("your answer "+ arr.options[userAns])
+function play(arr) {
+  var userAns = readlineSync.keyInSelect(arr.options, arr.question);
+  if (userAns !== -1) {
+    log("your answer " + arr.options[userAns])
   }
-    if(userAns == arr.answer){
-        log(right("you are right"));
-        score = score + 1;
-    }else if(userAns == -1){
-      log(info("you skip this question"));
-    }else{
-       log(wrong("you are wrong"));
-    }
-    return score;
+  if (userAns == arr.answer) {
+    log(right("you are right"));
+    score = score + 1;
+  } else if (userAns == -1) {
+    log(info("you skip this question"));
+  } else {
+    log(wrong("you are wrong"));
+  }
+  return score;
 }
-for(let i=0; i<questionArray.length; i++){
-    play(questionArray[i]);
+for (let i = 0; i < questionArray.length; i++) {
+  play(questionArray[i]);
 }
-log(info("your score is : "+score))
+log(info("your score is : " + score));
